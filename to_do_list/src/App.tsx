39922b17700a609389a175ec367a8c640e4a749b
Ian Wright <thewian27@gmail.com>
2024-01-02 01:36:50 +0300

    Setup
    Development environment and libraries

---
+++ to_do_list/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+    </div>
+  );
+}
+
+export default App;
